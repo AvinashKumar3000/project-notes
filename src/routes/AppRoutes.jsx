245c0417dda142bed17c0d7a144930a0cff0f6fb
Avi <avinashkumar@mviiitech.com>
2025-05-24 11:25:38 +0530

    updated latest code 24-05-2025 11:25:38.95

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,8 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
+import NotFound from "../pages/NotFound";
 
 export default function AppRoutes() {
   return (
-    <div>
-        
-    </div>
+    <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+    
+        <Route path="*" element={<NotFound/>}/>
+    </Routes>
   )
 }
